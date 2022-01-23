04dea86d61e30a739223c77ee76a2958ffb0d9dc
Christian Eiselt <dev@eiselt.ch>
2022-01-23 21:16:04 +0100

    add script

---
+++ .github/scripts/validate-branch-name.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+local_branch=$BRANCH_NAME
+
+valid_branch_regex="^(feature|bugfix|fix|hotfix|release)\/[a-z0-9._-]+$"
+
+message="Branch name '$local_branch' doesn't adhere to this contract: $valid_branch_regex."
+
+if [[ ! $local_branch =~ $valid_branch_regex ]]
+then
+    echo "$message"
+    exit 1
+fi
+
+exit 0
